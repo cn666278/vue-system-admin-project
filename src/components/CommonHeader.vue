5e2489c76629be0ff774353623005ccde888bbb8
Nuo Chen <chenn16@cardiff.ac.uk>
2024-03-21 00:17:59 +0800

    use and add menu icon button

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,11 +1,14 @@
 <template>
     <el-header>
         <div class="left-content">
-            <el-button type="text" icon="el-icon-menu"></el-button>
-            <el-button type="text" icon="el-icon-search"></el-button>
+            <el-button size="small">
+                <el-icon size="20">
+                    <Menu />
+                </el-icon>
+            </el-button>
         </div>
         <div class="right-content">
-            
+
         </div>
     </el-header>
 
