874bfa957e2a6279898c16629c27e0b6a9c3af19
Nuo Chen <chenn16@cardiff.ac.uk>
2024-03-22 08:52:10 +0800

    reset default style

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/less/index.less'
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
