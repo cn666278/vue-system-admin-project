e6b8a2adb584d889227545c2821528451b72b0d6
Nuo Chen <chenn16@cardiff.ac.uk>
2024-05-13 08:35:49 +0800

    [#] rebuild the code in Home.vue using setup

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -47,199 +47,189 @@
         <div ref="echart" style="height: 280px"></div>
       </el-card>
       <div class="graph">
-        <el-card style="height: 260px;">
-          <div ref="userEchart" style="height: 240px;"></div>
+        <el-card style="height: 260px">
+          <div ref="userEchart" style="height: 240px"></div>
         </el-card>
-        <el-card style="height: 260px;">
-          <div ref="videoEchart" style="height: 240px;"></div>
+        <el-card style="height: 260px">
+          <div ref="videoEchart" style="height: 240px"></div>
         </el-card>
       </div>
     </el-col>
   </el-row>
 </template>
 
-<script>
+<script setup>
 import {
-  defineComponent,
   onMounted,
   ref,
   getCurrentInstance,
   reactive,
 } from "vue";
-import axios from "axios";
+// import axios from "axios";
 import * as echarts from "echarts";
 
-export default defineComponent({
-  setup() {
-    // tableData使用let定义，因为后续会对其进行赋值, tableData is defined using let because it will be assigned later
-    let tableData = ref([]); // 双向绑定，使用ref包裹数组，实现响应式数据, Two-way binding, use ref to wrap the array to achieve responsive data
-    let countData = ref({});
-    // 今日购买，本月购买，总购买 Today's purchases, Monthly purchases, Total purchases
-    const tableLable = {
-      name: "Brand",
-      todayPurchase: "Today's purchases",
-      monthlyPurchase: "Monthly purchases",
-      totalPurchase: "Total purchases",
-    };
+// tableData使用let定义，因为后续会对其进行赋值, tableData is defined using let because it will be assigned later
+let tableData = ref([]); // 双向绑定，使用ref包裹数组，实现响应式数据, Two-way binding, use ref to wrap the array to achieve responsive data
+let countData = ref({});
+// 今日购买，本月购买，总购买 Today's purchases, Monthly purchases, Total purchases
+const tableLable = {
+  name: "Brand",
+  todayPurchase: "Today's purchases",
+  monthlyPurchase: "Monthly purchases",
+  totalPurchase: "Total purchases",
+};
 
-    // proxy是vue3.0提供的一个全局对象，可以通过getCurrentInstance()获取到当前实例，
-    // 再通过proxy.$api获取到api-fox模拟的接口数据
-    const { proxy } = getCurrentInstance();
-    const getTableList = async () => {
-      // 使用api-fox模拟请求数据
-      //   await axios
-      //     .get(
-      //       "https://mock.apifox.com/m1/4216854-0-default/api/home/getTableData"
-      //     )
-      //     .then((res) => {
-      //       console.log(res);
-      //       if (res.data.code === 200) {
-      //         tableData.value = res.data.data;
-      //       }
-      //     });
-      let res = await proxy.$api.getTableData();
-      //   console.log(res);
-      tableData.value = res;
-    };
-    // 获取首页count统计数据
-    const getCountData = async () => {
-      let res = await proxy.$api.getCountData();
-      countData.value = res;
-    };
-    // 关于echarts的渲染部分，可以参考echarts官网的文档
-    let xOptions = reactive({
-      // 图例文字颜色
-      textStyle: {
-        color: "#333",
-      },
-      grid: {
-        left: "20%",
-      },
-      // 提示框
-      tooltip: {
-        trigger: "axis",
-      },
-      xAxis: {
-        type: "category", // 类目轴
-        data: [],
-        axisLine: {
-          lineStyle: {
-            color: "#17b3a3",
-          },
-        },
-        axisLabel: {
-          interval: 0,
-          color: "#333",
-        },
-      },
-      yAxis: [
-        {
-          type: "value",
-          axisLine: {
-            lineStyle: {
-              color: "#17b3a3",
-            },
-          },
-        },
-      ],
-      color: ["#2ec7c9", "#b6a2de", "#5ab1ef", "#ffb980", "#d87a80", "#8d98b3"],
-      series: [],
-    });
-    let pieOptions = reactive({
-      tooltip: {
-        trigger: "item",
-      },
-      color: [
-        "#0f78f4",
-        "#dd536b",
-        "#9462e5",
-        "#a6a6a6",
-        "#e1bb22",
-        "#39c362",
-        "#3ed1cf",
-      ],
-      series: [],
-    });
-    // 定义响应式数据 !!
-    let orderData = reactive({
-      xData: [],
-      series: [],
-    });
-    let userData = reactive({
-      xData: [],
-      series: [],
-    });
-    let videoData = reactive({
-      series: [],
-    });
-
-    // 获取echarts数据
-    const getEchartData = async () => {
-      let result = await proxy.$api.getEchartData();
-      // console.log(result);
-      // let { orderData, userData, videoData } = result; // 解构赋值后不具有响应式，因为result是一个普通对象 
-      let res = result.orderData;
-      let userRes = result.userData;
-      let videoRes = result.videoData;
-      orderData.xData = res.date;
-      const keyArray = Object.keys(res.data[0]);
-      const series = []
-      keyArray.forEach((key) => {
-        series.push({
-          name: key,
-          data: res.data.map((item) => item[key]),
-          type: "line",
-        });
-      });
-      orderData.series = series;
-      xOptions.xAxis.data = orderData.xData;
-      xOptions.series = orderData.series;
-      // 渲染echarts: orderData
-      let hEcharts = echarts.init(proxy.$refs['echart']); // 获取dom节点 ref="echart"
-      hEcharts.setOption(xOptions);
-
-      // 渲染柱状图: userData
-      userData.xData = userRes.map((item) => item.date);
-      userData.series = [
-        {
-          name: "New users",
-          data: userRes.map((item) => item.new),
-          type: "bar",
-        },
-        {
-          name: "Active users",
-          data: userRes.map((item) => item.active),
-          type: "bar",
-        },
-      ];
-      xOptions.xAxis.data = userData.xData;
-      xOptions.series = userData.series;
-      let userEcharts = echarts.init(proxy.$refs['userEchart']); // 获取dom节点 ref="userEchart" 
-      userEcharts.setOption(xOptions);
-
-      // 渲染饼图: videoData
-      videoData.series = [
-        {
-          data: videoRes,
-          type: "pie",
-        }
-      ];
-      pieOptions.series = videoData.series;
-      let videoEcharts = echarts.init(proxy.$refs['videoEchart']); // 获取dom节点 ref="videoEchart"
-      videoEcharts.setOption(pieOptions);
-    };
-
-    onMounted(() => {
-      getTableList();
-      getCountData();
-      getEchartData();
-    });
-    return {
-      tableData,
-      tableLable,
-      countData,
-    };
+// proxy是vue3.0提供的一个全局对象，可以通过getCurrentInstance()获取到当前实例，
+// 再通过proxy.$api获取到api-fox模拟的接口数据
+const { proxy } = getCurrentInstance();
+const getTableList = async () => {
+  // 使用api-fox模拟请求数据
+  //   await axios
+  //     .get(
+  //       "https://mock.apifox.com/m1/4216854-0-default/api/home/getTableData"
+  //     )
+  //     .then((res) => {
+  //       console.log(res);
+  //       if (res.data.code === 200) {
+  //         tableData.value = res.data.data;
+  //       }
+  //     });
+  let res = await proxy.$api.getTableData();
+  //   console.log(res);
+  tableData.value = res;
+};
+// 获取首页count统计数据
+const getCountData = async () => {
+  let res = await proxy.$api.getCountData();
+  countData.value = res;
+};
+// 关于echarts的渲染部分，可以参考echarts官网的文档
+let xOptions = reactive({
+  // 图例文字颜色
+  textStyle: {
+    color: "#333",
   },
+  grid: {
+    left: "20%",
+  },
+  // 提示框
+  tooltip: {
+    trigger: "axis",
+  },
+  xAxis: {
+    type: "category", // 类目轴
+    data: [],
+    axisLine: {
+      lineStyle: {
+        color: "#17b3a3",
+      },
+    },
+    axisLabel: {
+      interval: 0,
+      color: "#333",
+    },
+  },
+  yAxis: [
+    {
+      type: "value",
+      axisLine: {
+        lineStyle: {
+          color: "#17b3a3",
+        },
+      },
+    },
+  ],
+  color: ["#2ec7c9", "#b6a2de", "#5ab1ef", "#ffb980", "#d87a80", "#8d98b3"],
+  series: [],
+});
+let pieOptions = reactive({
+  tooltip: {
+    trigger: "item",
+  },
+  color: [
+    "#0f78f4",
+    "#dd536b",
+    "#9462e5",
+    "#a6a6a6",
+    "#e1bb22",
+    "#39c362",
+    "#3ed1cf",
+  ],
+  series: [],
+});
+// 定义响应式数据 !!
+let orderData = reactive({
+  xData: [],
+  series: [],
+});
+let userData = reactive({
+  xData: [],
+  series: [],
+});
+let videoData = reactive({
+  series: [],
+});
+
+// 获取echarts数据
+const getEchartData = async () => {
+  let result = await proxy.$api.getEchartData();
+  // console.log(result);
+  // let { orderData, userData, videoData } = result; // 解构赋值后不具有响应式，因为result是一个普通对象
+  let res = result.orderData;
+  let userRes = result.userData;
+  let videoRes = result.videoData;
+  orderData.xData = res.date;
+  const keyArray = Object.keys(res.data[0]);
+  const series = [];
+  keyArray.forEach((key) => {
+    series.push({
+      name: key,
+      data: res.data.map((item) => item[key]),
+      type: "line",
+    });
+  });
+  orderData.series = series;
+  xOptions.xAxis.data = orderData.xData;
+  xOptions.series = orderData.series;
+  // 渲染echarts: orderData
+  let hEcharts = echarts.init(proxy.$refs["echart"]); // 获取dom节点 ref="echart"
+  hEcharts.setOption(xOptions);
+
+  // 渲染柱状图: userData
+  userData.xData = userRes.map((item) => item.date);
+  userData.series = [
+    {
+      name: "New users",
+      data: userRes.map((item) => item.new),
+      type: "bar",
+    },
+    {
+      name: "Active users",
+      data: userRes.map((item) => item.active),
+      type: "bar",
+    },
+  ];
+  xOptions.xAxis.data = userData.xData;
+  xOptions.series = userData.series;
+  let userEcharts = echarts.init(proxy.$refs["userEchart"]); // 获取dom节点 ref="userEchart"
+  userEcharts.setOption(xOptions);
+
+  // 渲染饼图: videoData
+  videoData.series = [
+    {
+      data: videoRes,
+      type: "pie",
+    },
+  ];
+  pieOptions.series = videoData.series;
+  let videoEcharts = echarts.init(proxy.$refs["videoEchart"]); // 获取dom节点 ref="videoEchart"
+  videoEcharts.setOption(pieOptions);
+};
+
+onMounted(() => {
+  getTableList();
+  getCountData();
+  getEchartData();
 });
 </script>
 
