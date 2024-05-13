f545c1698fc81b5aab6e1f53e186dab2d2d1fc6b
Nuo Chen <chenn16@cardiff.ac.uk>
2024-05-13 08:50:35 +0800

    [#] rebuild the code in User.vue using setup

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -150,7 +150,7 @@
   </el-dialog>
 </template>
 
-<script>
+<script setup>
 import { ElMessage } from "element-plus";
 import {
   defineComponent,
@@ -160,199 +160,178 @@ import {
   reactive,
 } from "vue";
 
-export default defineComponent({
-  setup() {
-    const { proxy } = getCurrentInstance(); // Get the instance object
-    const list = ref([]);
-    const tableLabel = reactive([
-      {
-        prop: "name",
-        label: "Name",
-      },
-      {
-        prop: "age",
-        label: "Age",
-      },
-      {
-        prop: "sexLabel",
-        label: "Gender",
-      },
-      {
-        prop: "birth",
-        label: "Date of Birth",
-        width: 200,
-      },
-      {
-        prop: "addr",
-        label: "Address",
-        width: 320,
-      },
-    ]);
-    onMounted(() => {
-      getUserData(config);
-    });
-    const config = reactive({
-      total: 0,
-      page: 1,
-      name: "",
-    });
-    // Get user data, and update the total number of data
-    const getUserData = async (config) => {
-      let res = await proxy.$api.getUserData(config);
-      //   console.log(res);
-      config.total = res.count;
-      list.value = res.list.map((item) => {
-        item.sexLabel = item.sex == 0 ? "Female" : "Male";
-        return item;
-      });
-    };
-    const changePage = (page) => {
-      config.page = page;
-      getUserData(config); // Call the API again, and the data will be updated
-    };
-    // Search form data
-    const formInline = reactive({
-      keyword: "",
-    });
-    const handleSearch = () => {
-      config.name = formInline.keyword;
-      getUserData(config); // Call the API again, and the data will be updated
-    };
-    // Dialog, 控制模态框的显示隐藏
-    const dialogVisible = ref(false);
-    const handleClose = (done) => {
-      ElMessageBox.confirm("Are you sure to close this dialog?")
-        .then(() => {
-          proxy.$refs.userForm.resetFields(); // Reset the form
-          done(); // Close the dialog
-        })
-        .catch(() => {
-          // catch error
-        });
-    };
-    // 添加用户的表单数据
-    const formUser = reactive({
-      name: "", // 用户名
-      age: "", // 年龄
-      sex: "", // 性别
-      birth: "", // 出生日期
-      addr: "", // 地址
-    });
-    // 时间格式化
-    // 也可以使用element-plus的日期格式化: value-format="YYYY-MM-DD"
-    // https://element-plus.org/en-US/component/date-picker.html#date-format
-    // 这里使用原生的
-    const timeFormat = (time) => {
-      var time = new Date(time);
-      var year = time.getFullYear();
-      var month = time.getMonth() + 1; // 月份是从0开始的
-      var date = time.getDate(); // 获取日: 1-31
-      function addZero(num) {
-        return num < 10 ? "0" + num : num; // 如果小于10,在前面加0
-      }
-      return year + "-" + addZero(month) + "-" + addZero(date); // 返回格式化的日期: YYYY-MM-DD
-    };
-    // 添加用户
-    const onSubmit = () => {
-      // 验证
-      // 验证有两种写法，在el-form-item上写rules，或者统一管理rules
-      // 参照: https://element-plus.org/en-US/component/form.html#form-validation
-      proxy.$refs.userForm.validate(async (valid) => {
-        if (valid) {
-          if (action.value == "add") {
-            formUser.birth = timeFormat(formUser.birth); // 格式化日期
-            let res = await proxy.$api.addUser(formUser);
-            if (res) {
-              // console.log(res)
-              dialogVisible.value = false;
-              proxy.$refs.userForm.resetFields(); // 重置表单
-              getUserData(config); // 更新数据
-            }
-          } else {
-            // 编辑的接口
-            // console.log(formUser);
-            formUser.sex = formUser.sex == "Male" ? 1 : 0; // 根据后端接口要求，转换性别为数字
-            formUser.birth = timeFormat(formUser.birth); // 格式化日期
-            let res = await proxy.$api.editUser(formUser);
-            if (res) {
-              dialogVisible.value = false;
-              proxy.$refs.userForm.resetFields(); // 重置表单
-              getUserData(config); // 更新数据
-            }
-          }
-        } else {
-          ElMessage({
-            showClose: true,
-            message: "Please fill in the form correctly",
-            type: "error",
-          });
-        }
-      });
-    };
-    // 取消添加用户,并重置表单
-    const handelCancel = () => {
-      dialogVisible.value = false;
-      proxy.$refs.userForm.resetFields(); // 重置表单
-    };
-    // 区分编辑和添加用户
-    const action = ref("add");
-    // 新增用户
-    const handleAdd = () => {
-      action.value = "add";
-      dialogVisible.value = true;
-    };
-    // 编辑用户
-    const handleEdit = (row) => {
-      console.log(row);
-      action.value = "edit";
-      dialogVisible.value = true;
-      row.sex = row.sex == 1 ? "Male" : "Female"; // 性别转换
-      // 这里的proxy是当前实例
-      // proxy.$nextTick的作用是在状态修改后等待dom更新后，再执行回调函数，否则当取消编辑时，表单数据不会重置
-      // https://vuejs.org/guide/essentials/reactivity-fundamentals#dom-update-timing
-      proxy.$nextTick(() => {
-        Object.assign(formUser, row); // 浅拷贝
-      });
-    };
-    // 删除用户
-    const handelDelete = (row) => {
-      ElMessageBox.confirm("Are you sure to delete user (" + row.name + ")?")
-        .then(async () => {
-          await proxy.$api.deleteUser({ id: row.id });
-          ElMessage({
-            showClose: true,
-            message: "Delete successfully",
-            type: "success",
-          });
-          getUserData(config); // 更新数据
-        })
-        .catch(() => {
-          // catch error
-        });
-    };
-    // 批量删除用户 - todo
-    const handelDeleteAll = () => {
-      console.log("delete all");
-    };
-    return {
-      list,
-      tableLabel,
-      config,
-      changePage,
-      formInline,
-      handleSearch,
-      dialogVisible,
-      handleClose,
-      formUser,
-      onSubmit,
-      handelCancel,
-      action,
-      handleAdd,
-      handleEdit,
-      handelDelete,
-    };
+const { proxy } = getCurrentInstance(); // Get the instance object
+const list = ref([]);
+const tableLabel = reactive([
+  {
+    prop: "name",
+    label: "Name",
   },
+  {
+    prop: "age",
+    label: "Age",
+  },
+  {
+    prop: "sexLabel",
+    label: "Gender",
+  },
+  {
+    prop: "birth",
+    label: "Date of Birth",
+    width: 200,
+  },
+  {
+    prop: "addr",
+    label: "Address",
+    width: 320,
+  },
+]);
+onMounted(() => {
+  getUserData(config);
 });
+const config = reactive({
+  total: 0,
+  page: 1,
+  name: "",
+});
+// Get user data, and update the total number of data
+const getUserData = async (config) => {
+  let res = await proxy.$api.getUserData(config);
+  //   console.log(res);
+  config.total = res.count;
+  list.value = res.list.map((item) => {
+    item.sexLabel = item.sex == 0 ? "Female" : "Male";
+    return item;
+  });
+};
+const changePage = (page) => {
+  config.page = page;
+  getUserData(config); // Call the API again, and the data will be updated
+};
+// Search form data
+const formInline = reactive({
+  keyword: "",
+});
+const handleSearch = () => {
+  config.name = formInline.keyword;
+  getUserData(config); // Call the API again, and the data will be updated
+};
+// Dialog, 控制模态框的显示隐藏
+const dialogVisible = ref(false);
+const handleClose = (done) => {
+  ElMessageBox.confirm("Are you sure to close this dialog?")
+    .then(() => {
+      proxy.$refs.userForm.resetFields(); // Reset the form
+      done(); // Close the dialog
+    })
+    .catch(() => {
+      // catch error
+    });
+};
+// 添加用户的表单数据
+const formUser = reactive({
+  name: "", // 用户名
+  age: "", // 年龄
+  sex: "", // 性别
+  birth: "", // 出生日期
+  addr: "", // 地址
+});
+// 时间格式化
+// 也可以使用element-plus的日期格式化: value-format="YYYY-MM-DD"
+// https://element-plus.org/en-US/component/date-picker.html#date-format
+// 这里使用原生的
+const timeFormat = (time) => {
+  var time = new Date(time);
+  var year = time.getFullYear();
+  var month = time.getMonth() + 1; // 月份是从0开始的
+  var date = time.getDate(); // 获取日: 1-31
+  function addZero(num) {
+    return num < 10 ? "0" + num : num; // 如果小于10,在前面加0
+  }
+  return year + "-" + addZero(month) + "-" + addZero(date); // 返回格式化的日期: YYYY-MM-DD
+};
+// 添加用户
+const onSubmit = () => {
+  // 验证
+  // 验证有两种写法，在el-form-item上写rules，或者统一管理rules
+  // 参照: https://element-plus.org/en-US/component/form.html#form-validation
+  proxy.$refs.userForm.validate(async (valid) => {
+    if (valid) {
+      if (action.value == "add") {
+        formUser.birth = timeFormat(formUser.birth); // 格式化日期
+        let res = await proxy.$api.addUser(formUser);
+        if (res) {
+          // console.log(res)
+          dialogVisible.value = false;
+          proxy.$refs.userForm.resetFields(); // 重置表单
+          getUserData(config); // 更新数据
+        }
+      } else {
+        // 编辑的接口
+        // console.log(formUser);
+        formUser.sex = formUser.sex == "Male" ? 1 : 0; // 根据后端接口要求，转换性别为数字
+        formUser.birth = timeFormat(formUser.birth); // 格式化日期
+        let res = await proxy.$api.editUser(formUser);
+        if (res) {
+          dialogVisible.value = false;
+          proxy.$refs.userForm.resetFields(); // 重置表单
+          getUserData(config); // 更新数据
+        }
+      }
+    } else {
+      ElMessage({
+        showClose: true,
+        message: "Please fill in the form correctly",
+        type: "error",
+      });
+    }
+  });
+};
+// 取消添加用户,并重置表单
+const handelCancel = () => {
+  dialogVisible.value = false;
+  proxy.$refs.userForm.resetFields(); // 重置表单
+};
+// 区分编辑和添加用户
+const action = ref("add");
+// 新增用户
+const handleAdd = () => {
+  action.value = "add";
+  dialogVisible.value = true;
+};
+// 编辑用户
+const handleEdit = (row) => {
+  console.log(row);
+  action.value = "edit";
+  dialogVisible.value = true;
+  row.sex = row.sex == 1 ? "Male" : "Female"; // 性别转换
+  // 这里的proxy是当前实例
+  // proxy.$nextTick的作用是在状态修改后等待dom更新后，再执行回调函数，否则当取消编辑时，表单数据不会重置
+  // https://vuejs.org/guide/essentials/reactivity-fundamentals#dom-update-timing
+  proxy.$nextTick(() => {
+    Object.assign(formUser, row); // 浅拷贝
+  });
+};
+// 删除用户
+const handelDelete = (row) => {
+  ElMessageBox.confirm("Are you sure to delete user (" + row.name + ")?")
+    .then(async () => {
+      await proxy.$api.deleteUser({ id: row.id });
+      ElMessage({
+        showClose: true,
+        message: "Delete successfully",
+        type: "success",
+      });
+      getUserData(config); // 更新数据
+    })
+    .catch(() => {
+      // catch error
+    });
+};
+// 批量删除用户 - todo
+const handelDeleteAll = () => {
+  console.log("delete all");
+};
 </script>
 
 <style lang="less" scoped>
